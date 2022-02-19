34347f7b03b4ae792d3d2aebeb84224a12ee82d9
ÖMER ÖZTİN <omeroztin@gmail.com>
2022-02-19 20:02:54 +0300

    Büyük Değişiklikler

---
+++ Assets/Scripts/WarriorMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorMovement : MonoBehaviour
+{
+    public float movementSpeed = 1;
+    public float jumpForce = 20;
+    public float horizontalMove = 0f;
+
+    public Animator _animator = new Animator();
+    private Rigidbody2D _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            _animator.SetBool("isJumping", true);
+        }
+        _animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Zemin")
+        {
+            _animator.SetBool("isJumping", false);
+        }
+    }
+}
